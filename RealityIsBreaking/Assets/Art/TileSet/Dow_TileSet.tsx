6044a87dc859581040edd047ab9ce51015778030
Oknaa <mohammadlaadidaoui@gmail.com>
2022-05-20 21:30:30 +0100

    Changed the color palette of the tileset, and Added the first half of the level

---
+++ RealityIsBreaking/Assets/Art/TileSet/Dow_TileSet.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Dow_TileMap" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="Dow_TileSet.png" width="320" height="320"/>
+ <wangsets>
+  <wangset name="Terrains" type="corner" tile="-1">
+   <wangcolor name="Walls" color="#ff0000" tile="0" probability="1"/>
+   <wangtile tileid="167" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="168" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="169" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="170" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="171" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="187" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="189" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="190" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="191" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="207" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="208" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="210" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="211" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="228" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="230" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="247" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="248" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="250" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="251" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="267" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="271" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="287" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="288" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="289" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="290" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="291" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
