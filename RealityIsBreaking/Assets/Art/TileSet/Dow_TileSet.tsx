ff4d1516c05c1fa56eb164940e901c2862e769e5
Oknaa <mohammadlaadidaoui@gmail.com>
2022-05-21 01:22:34 +0100

    Tweaked the art, Added the final room, Fixed the player colours

--- RealityIsBreaking/Assets/Art/TileSet/Dow_TileSet.tsx
+++ RealityIsBreaking/Assets/Art/TileSet/Dow_TileSet.tsx
@@ -1,35 +1,60 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.5" name="Dow_TileMap" tilewidth="16" tileheight="16" tilecount="400" columns="20">
  <image source="Dow_TileSet.png" width="320" height="320"/>
+ <tile id="188" probability="0.01"/>
+ <tile id="189" probability="0.1"/>
+ <tile id="190" probability="0.01"/>
+ <tile id="209" probability="0.01"/>
+ <tile id="229" probability="0.01"/>
+ <tile id="249" probability="0.01"/>
  <wangsets>
-  <wangset name="Terrains" type="corner" tile="-1">
-   <wangcolor name="Walls" color="#ff0000" tile="0" probability="1"/>
+  <wangset name="Terrains" type="corner" tile="170">
+   <wangcolor name="Walls" color="#00ff00" tile="170" probability="1"/>
    <wangtile tileid="167" wangid="0,1,0,0,0,1,0,1"/>
    <wangtile tileid="168" wangid="0,1,0,0,0,0,0,1"/>
    <wangtile tileid="169" wangid="0,1,0,0,0,0,0,1"/>
    <wangtile tileid="170" wangid="0,1,0,0,0,0,0,1"/>
    <wangtile tileid="171" wangid="0,1,0,1,0,0,0,1"/>
    <wangtile tileid="187" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="188" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="189" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="190" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="191" wangid="0,1,0,1,0,0,0,0"/>
    <wangtile tileid="207" wangid="0,0,0,1,0,1,0,1"/>
    <wangtile tileid="208" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="209" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="210" wangid="0,0,0,1,0,0,0,0"/>
    <wangtile tileid="211" wangid="0,1,0,1,0,1,0,0"/>
    <wangtile tileid="228" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="229" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="230" wangid="0,1,0,1,0,0,0,0"/>
    <wangtile tileid="247" wangid="0,1,0,0,0,1,0,1"/>
    <wangtile tileid="248" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="249" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="250" wangid="0,1,0,0,0,0,0,0"/>
    <wangtile tileid="251" wangid="0,1,0,1,0,0,0,1"/>
    <wangtile tileid="267" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="268" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="269" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="270" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="271" wangid="0,1,0,1,0,0,0,0"/>
    <wangtile tileid="287" wangid="0,0,0,1,0,1,0,1"/>
    <wangtile tileid="288" wangid="0,0,0,1,0,1,0,0"/>
    <wangtile tileid="289" wangid="0,0,0,1,0,1,0,0"/>
    <wangtile tileid="290" wangid="0,0,0,1,0,1,0,0"/>
    <wangtile tileid="291" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+  <wangset name="Background" type="corner" tile="190">
+   <wangcolor name="" color="#ff0000" tile="190" probability="1"/>
+   <wangtile tileid="188" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="189" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="190" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="209" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="229" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="249" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="268" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="269" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="270" wangid="0,1,0,1,0,1,0,1"/>
   </wangset>
  </wangsets>
 </tileset>
