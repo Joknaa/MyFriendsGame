9351a232f5d0b4071b7048c293635ba532e24e26
Oknaa <mohammadlaadidaoui@gmail.com>
2022-05-21 18:56:49 +0100

    Added the HUD, Health bar, and Damage

---
+++ RealityIsBreaking/Assets/_Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/* http://www.Mousawi.Dev By @AbdullaMousawi*/
+public class Health : MonoBehaviour {
+    public Image[] healthPoints;
+
+    public float health, maxHealth = 100;
+
+    private void Start() {
+        health = maxHealth;
+    }
+
+    private void Update() {
+        if (health > maxHealth) health = maxHealth;
+        
+        for (int i = 0; i < healthPoints.Length; i++) {
+            healthPoints[i].enabled = health > i * maxHealth / healthPoints.Length;
+        }
+    }
+
+    public void Damage(float damagePoints) {
+        if (health > 0) health -= damagePoints;
+    }
+
+    public void Heal(float healingPoints) {
+        if (health < maxHealth) health += healingPoints;
+    }
+}
