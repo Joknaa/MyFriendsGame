84027b71dd7a2c986d3ec213eaf920bcaa85174c
ZiadRbai <ziadrbai@gmail.com>
2022-05-22 21:06:25 +0100

    Commented gun knockback

--- RealityIsBreaking/Assets/_Scripts/Player/Gun.cs
+++ RealityIsBreaking/Assets/_Scripts/Player/Gun.cs
@@ -7,25 +7,33 @@ namespace Reality {
     public class Gun : MonoBehaviour {
         [SerializeField] private GameObject bullet;
         [SerializeField] private float bulletForce = 20f;
+        [SerializeField] private float knockBackForce = 5f;
+        
+
 
         [SerializeField] private AudioSource _source;
         [SerializeField] private AudioClip[] SFX;
 
         private GameObject player;
+        private Rigidbody2D playerRigidbody;
 
 
         private Vector3 mousePosition;
+        SpriteRenderer cursorSprite;
         private Camera cam;
 
 
         void Start() {
             cam = Camera.main;
             player = transform.parent.gameObject;
+            playerRigidbody = player.GetComponent<Rigidbody2D>();
+            cursorSprite = GetComponent<SpriteRenderer>();
         }
 
         void Update() {
             if (!GameStateController.Instance.IsPlaying()) return;
 
+            cursorSprite.enabled = true;
             MoveCursor();
             FireBullet();
             //MoveGun();
@@ -38,7 +46,7 @@ namespace Reality {
             //Sets cursor position
             transform.position = mousePosition;
 
-            Debug.DrawLine(player.transform.position, mousePosition, Color.red);
+            //Debug.DrawLine(player.transform.position, mousePosition, Color.red);
         }
 
 
@@ -58,8 +66,51 @@ namespace Reality {
             bulletRb.rotation = angle;
             bulletRb.AddForce(((Vector2) transform.position - (Vector2) player.transform.position).normalized * bulletForce, ForceMode2D.Impulse);
 
+
+            //KNOCKBACK
+            //float knockBackDirection = -(transform.position.x - player.transform.position.x);
+
+            //if (knockBackDirection > 0)
+            //{
+            //    StartCoroutine(KnockBackVelocity(1));
+            //    //playerRigidbody.AddForce(new Vector2( knockBackForce, 0f), ForceMode2D.Impulse);
+            //}
+            //else
+            //{
+            //    //playerRigidbody.AddForce(new Vector2( -knockBackForce, 0f), ForceMode2D.Impulse);
+            //    StartCoroutine(KnockBackVelocity(-1));
+            //}
+            
+
             _source.PlayOneShot(SFX[0]);
         }
+
+        IEnumerator KnockBackVelocity(int direction)
+        {
+            float i = 0.01f;
+            while (knockBackForce > i)
+            {
+                playerRigidbody.velocity = new Vector2((direction*knockBackForce )/ i, playerRigidbody.velocity.y);
+                i = i + Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            playerRigidbody.velocity = Vector2.zero;
+            yield return null;
+        }
+
+        IEnumerator KnockBackTransform(int direction)
+        {
+            float i = 0.01f;
+            while (knockBackForce > i)
+            {
+                playerRigidbody.velocity = new Vector2((direction * knockBackForce) / i, playerRigidbody.velocity.y);
+                i = i + Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            playerRigidbody.velocity = Vector2.zero;
+            yield return null;
+        }
+
 
         private float getShootingDirection() {
             Vector2 lookDir = (Vector2) transform.position - (Vector2) player.transform.position;
